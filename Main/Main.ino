1ba0ba4f2879554ff82b8747c67e68f5e9c8d8dc
Sebastian <sebasamol@gmail.com>
2021-09-28 19:13:22 +0200

    Konfiguracja ESP8266 jako server

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,59 +4,33 @@
 //#include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
 
+const char* ssid = "CGA2121_7QPJvFa";
+const char* password = "pUCafjBwtY9rcyDeb6";  
+
+WiFiServer server(80);
 
 //Adafruit_BME280 bme;
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
   Serial.println();
 
-  WiFi.begin("CGA2121_7QPJvFa", "pUCafjBwtY9rcyDeb6");
-
+  
+  WiFi.begin(ssid, password);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
+  Serial.println(" Connected");
 
-  Serial.print("Connected, IP address: ");
+  server.begin();
+ 
   Serial.println(WiFi.localIP());
 
 } 
 
-/*
 
-void measure_temp() 
-{
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println("*C");
-}
-
-
-void measure_hum()
-{
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println("%");
-}
-
-
-void measure_press()
-{
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println("hPa");
-}
-
-
-void loop() {
- // measure_temp();
- // measure_hum();
- // measure_press(); 
- // delay(1000);
-
-} */
 void loop() {}
