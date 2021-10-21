f55f1ca25aee70f05908dcf303ec1785f56f6b1c
Sebastian <sebasamol@gmail.com>
2021-10-21 22:04:41 +0200

    Testowanie wyświetlacza OLED

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,8 +1,19 @@
 
-//#include <Wire.h>
 //#include <Adafruit_Sensor.h>
 //#include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 //szerokosc ekranu
+#define SCREEN_HEIGHT 48 //wysokosc ekranu
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 
 const char* ssid = "CGA2121_7QPJvFa";
 const char* password = "pUCafjBwtY9rcyDeb6";  
@@ -13,6 +24,12 @@ WiFiServer server(80);
 
 void setup() 
 {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
+  display.display();
+  delay(2000);
+  
+  Wire.begin();
   Serial.begin(115200);
   Serial.println();
 
@@ -30,7 +47,23 @@ void setup()
  
   Serial.println(WiFi.localIP());
 
+
+  display.clearDisplay();
+ 
+  // text display tests
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(32,3);
+  display.println("27.8");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 } 
 
 
-void loop() {}
+void loop() {
+  
+  
+  
+  
+  }
